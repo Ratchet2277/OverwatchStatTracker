e7c9b6ebc33902f77e3df2c08529723d9eafe304
Jérémy Saudemont <jeremysaudemont@gmail.com>
2021-06-12 17:11:11 +0200

    Helper to get Current Season

--- OverwatchTracker/Helpers/SeasonHelper.cs
+++ OverwatchTracker/Helpers/SeasonHelper.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApplication.Helpers
+﻿using System.Linq;
+using DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.Helpers
 {
     public class SeasonHelper
     {
+        public static Season GetLastSeason(DbSet<Season> dbSet)
+        {
+            return dbSet.OrderByDescending(s => s.Number).First();
+        }
     }
 }
