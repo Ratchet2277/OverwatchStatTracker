41500b7bb989a445a9cfa9a8cfe2394cba549fda
Jérémy <jeremy.saudemont@nowwweb.com>
2021-06-12 12:28:11 +0200

    Add SquadMember Autocompletion

---
+++ OverwatchTracker/Controllers/SquadMemberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class SquadMemberController : BaseController
+    {
+        public SquadMemberController(TrackerContext context, ILogger<SquadMemberController> logger) : base(context,
+            logger)
+        {
+        }
+
+        public async Task<Select2Result> Search(string term)
+        {
+            Select2Result result = new(await Context.SquadMembers.Where(s => s.Name.Contains(term))
+                .Select(s => s.Name)
+                .ToListAsync());
+            return result;
+        }
+    }
+}
---
+++ OverwatchTracker/Models/Select2Result.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WebApplication.Models
+{
+    public class Select2Result
+    {
+        [JsonPropertyName("results")] public List<Select2ResultItem> Results { get; set; } = new();
+
+
+        public Select2Result(List<string> list)
+        {
+            foreach (string s in list)
+            {
+                Results.Add(new Select2ResultItem(){Id = s, Text = s});
+            }
+        }
+
+        public Select2Result(Dictionary<int, string> dictionary)
+        {
+            foreach (KeyValuePair<int,string> pair in dictionary)
+            {
+                Results.Add(new Select2ResultItem{Id = pair.Key.ToString(), Text = pair.Value});
+            }
+        }
+    }
+}
