7772aed711d46a5944521d8c512eccc585eb149b
Jérémy Saudemont <jeremysaudemont@gmail.com>
2021-06-13 03:29:09 +0200

    Clean Warning

--- OverwatchTracker/Controllers/SquadMemberController.cs
+++ OverwatchTracker/Controllers/SquadMemberController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using DAL;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebApplication.Models;
