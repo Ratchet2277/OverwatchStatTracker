b1588846b545bb2cae0c33f1676d3fbb87c6678a
Jérémy Saudemont <jersaudemont@gmail.com>
2021-07-26 10:24:52 +0200

    Move Ajax call to partial a class

--- OverwatchTracker/Controllers/GameController.cs
+++ OverwatchTracker/Controllers/GameController.cs
@@ -1,24 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using DAL;
 using DomainModel;
-using DomainModel.Types;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebApplication.Buisness;
-using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     [Authorize]
     [Route("Game")]
-    public class GameController : BaseController
+    public partial class GameController : BaseController
     {
         private readonly SeasonBuisness _seasonBuisness;
 
@@ -26,33 +23,6 @@ namespace WebApplication.Controllers
             SeasonBuisness seasonBuisness) : base(context, logger, userManager)
         {
             _seasonBuisness = seasonBuisness;
-        }
-
-        [ResponseCache(Duration = 3600)]
-        [HttpGet("MapList")]
-        public List<Map> MapList()
-        {
-            return _seasonBuisness.GetLastSeason().MapPool.OrderBy(m => m.Name).ToList();
-        }
-
-        [HttpGet("RoleList")]
-        [ResponseCache(Duration = 3600)]
-        public Dictionary<int, string> RoleList()
-        {
-            return Enum.GetValues(typeof(GameType)).Cast<GameType>().ToDictionary(t => (int) t, t => t.ToString());
-        }
-
-        [HttpGet("HeroList/{roleId:int?}")]
-        [ResponseCache(Duration = 3600, VaryByHeader = "roleId")]
-        public async Task<List<Hero>> HeroList(int? roleId)
-        {
-            var season = await Context.Seasons.OrderByDescending(s => s.Number).FirstAsync();
-            IEnumerable<Hero> query = season.HeroPool.OrderBy(h => h.Name);
-
-            if (Enum.TryParse(roleId.ToString(), out Role role) && season.HeroPool.Any(h => h.Role == role))
-                query = query.Where(h => h.Role == role);
-
-            return query.ToList();
         }
 
         [HttpPost]
